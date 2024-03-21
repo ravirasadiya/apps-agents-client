4bf76b683de83c5c4f73d52a97baa580c9767709
mcsquare005 <rasadiyaravi@gmail.com>
2024-03-21 11:05:47 +0530

    corrections

--- src/pages/settlements.tsx
+++ src/pages/settlements.tsx
@@ -3,7 +3,6 @@ import Head from "next/head";
 import Layout from "@/component/layouts/Layout";
 import { Box, Button, Grid, Modal } from "@mui/material";
 import Typography from '@mui/material/Typography';
-import moment from 'moment';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-daterangepicker/daterangepicker.css";
@@ -14,6 +13,7 @@ import MenuItem from '@mui/material/MenuItem';
 import Dialog from '@mui/material/Dialog';
 import SettlementsTabal from '@/component/Settlements/SettlementsTabal';
 import DateAndSelect from '@/component/dashboard/DateAndSelect';
+import moment from 'moment';
 
 
 
@@ -27,21 +27,7 @@ export default function Settlements() {
   //datepicker
   const [fromDate, setFromDate] = useState(new Date());
   const [toDate, setToDate] = useState(new Date());
-  const range = {
-    Today: [moment(), moment()],
-    Yesterday: [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-    'This Month': [moment().startOf('month'), moment().endOf('month')],
-    'Last Month': [
-      moment().subtract(1, 'month').startOf('month'),
-      moment().subtract(1, 'month').endOf('month'),
-    ],
-    'Last Year': [
-      moment().subtract(1, 'year').startOf('year'),
-      moment().subtract(1, 'year').endOf('year'),
-    ],
-  };
+  
 
   const handleEvent = (event: any, picker: any) => {
     setFromDate(picker.startDate._d.toISOString());
@@ -96,8 +82,7 @@ export default function Settlements() {
                 <Box component="form">
                   <Box className="date_min_prnt">
                     <DateRangePicker
-                      ranges={range}
-                      alwaysShowCalendars={true}
+                      initialSettings={{ startDate: '1/1/2014', endDate: '3/1/2014' }}
                       onEvent={handleEvent}
                     >
                       <button className='def_date_pickr'>
